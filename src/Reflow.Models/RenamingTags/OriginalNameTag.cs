5792b65853a18675b2bc319c953765379d3f49df
Hentov <h_hentov@yahoo.com>
2018-07-08 18:35:00 +0300

    Added original name tag

---
+++ src/Reflow.Models/RenamingTags/OriginalNameTag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Reflow.Contract.DTO;
+
+namespace Reflow.Models.RenamingTags
+{
+    public class OriginalNameTag : BaseTag
+    {
+        protected OriginalNameTag() : base(nameof(OriginalNameTag))
+        {
+        }
+
+        public override string Render(string fileName, IDictionary<string, ReflowFile> files)
+        {
+            return fileName;
+        }
+    }
+}
